9349a29b3b6d5412059375cf4853cdb30dfee001
itSpecialistMike <personal.hardworkin@gmail.com>
2025-09-29 17:43:28 +0700

    feat: плавные анимации изменения

--- #3 Прокрастинация/script.js
+++ #3 Прокрастинация/script.js
@@ -23,8 +23,8 @@ let image = document.querySelector('.image');
 
 button.addEventListener('click', function () {
     let randomElement = getRandomElement(phrases);
-    phrase.textContent = randomElement.text;
-    image.src = randomElement.image
+    smoothly(phrase, 'textContent', randomElement.text);
+    smoothly(image, 'src', randomElement.image);
 
     if (randomElement.text.length > 40) {
         advice.style.fontSize = '33px';
